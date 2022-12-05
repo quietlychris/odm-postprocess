975464842d0cd90c233f8856e6233edcc49dd68d
chrism <cmoran@cmoran.xyz>
2022-12-04 20:08:38 -0500

    Add initial support for a clap CLI

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,192 @@
+// CLI dep
+use clap::{value_parser, Arg, Command};
+// WebP processing deps
 use image::*;
 use serde_json::Value;
 use webp::*;
-
-use std::env;
+// General-use deps
 use std::error::Error;
-use std::path::{Path, PathBuf};
 use std::fs;
 use std::io::Write;
-
+use std::path::{Path, PathBuf};
+// Crate internals also used for MapLibre deployment
 mod summary;
 use crate::summary::*;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn main() -> Result<(), Box<dyn Error>> {
+    let cmd = Command::new("odm_postprocess")
+        .about("OpenDroneMap post-processing program for cmoran's drone pipeline")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            Command::new("height_to_px_res")
+                .about("Using the drone's height in meters, calculate the appropriate pixel resolution for NodeODM")
+                .arg(
+                    Arg::new("height")
+                        .short('H')
+                        .long("height")
+                        .help("Height of the drone in meters")
+                        .required(true)
+                        .value_parser(value_parser!(f64))
+                )
+                .arg(
+                        Arg::new("y_res")
+                            .short('y')
+                            .long("y_res")
+                            .help("Resolution of a photo in pixels along the y-axis (vertical)")
+                            .required(true)
+                            .value_parser(value_parser!(usize))
+                )
+                .arg(
+                    Arg::new("x_res")
+                        .short('x')
+                        .long("x_res")
+                        .help("Resolution of a photo in pixels along the x-axis (horizontal)")
+                        .required(true)
+                        .value_parser(value_parser!(usize))
+            )
+        )
+        .subcommand(
+            Command::new("convert")
+                .about("Convert an ODM post-process package to an upload-able package for a MapLibre site")
+                .arg(
+                    Arg::new("input-dir")
+                        .short('i')
+                        .long("input-dir")
+                        .help("<input-directory>")
+                        .required(true)
+                        .value_parser(value_parser!(std::path::PathBuf))
+                )
+                .arg(
+                    Arg::new("output-dir")
+                        .short('o')
+                        .long("output-dir")
+                        .help("<output-directory>")
+                        .required(true)
+                        .value_parser(value_parser!(std::path::PathBuf))
+                )
+                .arg(
+                    Arg::new("size-factor")
+                        .short('s')
+                        .long("size-factor")
+                        .help("Scale down from the original orthophoto [0.0-1.0]")
+                        .required(false)
+                        .default_value("0.2")
+                        .value_parser(value_parser!(f64))
+                )
+                .arg(
+                    Arg::new("webp-quality")
+                        .short('q')
+                        .long("webp-quality")
+                        .help("Quality of the WebP compression")
+                        .required(false)
+                        .default_value("90.0")
+                        .value_parser(value_parser!(f32))
+                )
 
-    let input_dir = args[1].parse::<PathBuf>().unwrap();
-    let output_dir = args[2].parse::<PathBuf>().unwrap();
+        );
 
-    if !output_dir.exists() {
-        std::fs::create_dir_all(&output_dir).unwrap();
+    let matches = cmd.get_matches();
+
+    match matches.subcommand() {
+        Some(("height_to_px_res", h2pxr_matches)) => {
+            let height = *h2pxr_matches.get_one::<f64>("height").unwrap();
+            let y_res = *h2pxr_matches.get_one::<usize>("y_res").unwrap();
+            let x_res = *h2pxr_matches.get_one::<usize>("x_res").unwrap();
+
+            // TO_DO
+
+            println!("{:?},{},{}", height, x_res, y_res);
+        }
+        Some(("convert", convert_matches)) => {
+            let input_dir = convert_matches
+                .get_one::<PathBuf>("input-dir")
+                .unwrap()
+                .to_owned();
+            let output_dir = convert_matches
+                .get_one::<PathBuf>("output-dir")
+                .unwrap()
+                .to_owned();
+            let size_factor = *convert_matches.get_one::<f64>("size-factor").unwrap();
+            let quality = *convert_matches.get_one::<f32>("webp-quality").unwrap();
+
+            // <-- CONVERT THE ORIGINAL ODM OUTPUT TO A WEBMAP-COMPATIBLE VERSION -->
+            if !output_dir.exists() {
+                std::fs::create_dir_all(&output_dir).unwrap();
+            }
+        
+            let input_json_path = input_dir
+                .join("odm_georeferencing")
+                .join("odm_georeferenced_model")
+                .with_extension("info.json");
+
+            println!("json_path: {:?}", input_json_path);
+            let (bounds, center) = get_bounds(input_json_path);
+            println!("Bounds: {:?}", bounds);
+        
+            let summary = Summary {
+                title: "".to_string(),
+                description: "".to_string(),
+                bounds,
+                center,
+            };
+            let j = serde_json::to_string_pretty(&summary).unwrap();
+            let path_summary_json = output_dir.join("summary").with_extension("json");
+            let mut summary_json = fs::File::create(path_summary_json).unwrap();
+            summary_json.write_all(j.as_bytes()).unwrap();
+        
+            process_orthophoto(input_dir, output_dir, size_factor, quality).unwrap();
+        }
+        _ => (),
     }
 
-    let input_json_path = input_dir
-        .join("odm_georeferencing")
-        .join("odm_georeferenced_model")
-        .with_extension("info.json");
-    // .with_extension("json");
-    println!("json_path: {:?}", input_json_path);
-    let (bounds, center) = get_bounds(input_json_path);
-    println!("Bounds: {:?}", bounds);
-
-    let summary  = Summary {title: "".to_string(), description: "".to_string(), bounds, center };
-    let j = serde_json::to_string_pretty(&summary).unwrap();
-    let path_summary_json = output_dir.join("summary").with_extension("json");
-    let mut summary_json = fs::File::create(path_summary_json).unwrap(); 
-    summary_json.write_all(j.as_bytes()).unwrap();   
-
-    process_orthophoto(&input_dir, &output_dir).unwrap();
+    Ok(())
 }
 
+/// Process the full-res ODM orthophoto package into a smaller web-compatible product
+fn process_orthophoto(
+    input_dir: PathBuf,
+    output_dir: PathBuf,
+    size_factor: f64,
+    quality: f32,
+) -> Result<(), Box<dyn Error>> {
+    let path_orthophoto = Path::new("odm_orthophoto").join("odm_orthophoto.png");
+    let input_path_orthophoto = input_dir.join(&path_orthophoto);
+
+    // Create a low-definition .webp version of the orthophoto .png
+    let mut rdr = image::io::Reader::open(&input_path_orthophoto)?.with_guessed_format()?;
+    // Orthographic .png mosaics routinely become too large for the default Reader
+    // We need to explicitly de-limit the Reader to accommodate those imagers
+    rdr.no_limits();
+
+    let img = match rdr.decode() {
+        Ok(img) => img,
+        Err(e) => panic!("Error: {:?} on {:?}", e, &input_path_orthophoto),
+    };
+    let (w, h) = img.dimensions();
+    // Save the full-def orthographic photo
+    img.save(output_dir.join("odm_orthophoto").with_extension("png"))
+        .unwrap();
+
+    let img = image::DynamicImage::ImageRgba8(imageops::resize(
+        &img,
+        (w as f64 * size_factor) as u32,
+        (h as f64 * size_factor) as u32,
+        imageops::FilterType::Triangle,
+    ));
+
+    let encoder: Encoder = Encoder::from_image(&img)?;
+    let webp: WebPMemory = encoder.encode(quality);
+
+    let output_path_orthophoto = output_dir.join("odm_orthophoto").with_extension("webp");
+
+    fs::write(&output_path_orthophoto, &*webp)?;
+
+    Ok(())
+}
+
+/// Get orthophoto bounds from the ODM JSON summary
 pub fn get_bounds(json_path: PathBuf) -> (Bounds, Center) {
     let json = std::fs::read_to_string(json_path).unwrap();
     let v: Value = serde_json::from_str(&json).unwrap();
@@ -75,44 +223,3 @@ pub fn get_bounds(json_path: PathBuf) -> (Bounds, Center) {
 
     (bounds, center)
 }
-
-fn process_orthophoto(input_dir: &Path, output_dir: &Path) -> Result<(), Box<dyn Error>> {
-
-    let path_orthophoto = Path::new("odm_orthophoto").join("odm_orthophoto.png");
-    let input_path_orthophoto = input_dir.join(&path_orthophoto);
-    
-    // Create a low-definition .webp version of the orthophoto .png 
-    
-    let mut rdr = image::io::Reader::open(&input_path_orthophoto)?.with_guessed_format()?;
-    // Orthographic .png mosaics routinely become too large for the default Reader
-    // We need to explicitly de-limit the Reader to accommodate those imagers
-    rdr.no_limits();
-
-    let img = match rdr.decode() {
-        Ok(img) => img,
-        Err(e) => panic!("Error: {:?} on {:?}", e, &input_path_orthophoto),
-    };
-    let (w, h) = img.dimensions();
-    let size_factor = 0.2;
-    // Save the full-def orthographic photo
-    img.save(output_dir.join("odm_orthophoto").with_extension("png")).unwrap();
-
-    let img = image::DynamicImage::ImageRgba8(imageops::resize(
-        &img,
-        (w as f64 * size_factor) as u32,
-        (h as f64 * size_factor) as u32,
-        imageops::FilterType::Triangle,
-    ));
-
-    let encoder: Encoder = Encoder::from_image(&img)?;
-    let webp_quality = 90.0;
-    let webp: WebPMemory = encoder.encode(webp_quality);
-
-    let output_path_orthophoto = output_dir.join("odm_orthophoto").with_extension("webp");
-
-    fs::write(&output_path_orthophoto, &*webp)?;
-
-    Ok(())
-}
-
-
--- src/summary.rs
+++ src/summary.rs
@@ -1,18 +1,17 @@
-
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Center {
     pub lat: f32,
-    pub lon: f32
+    pub lon: f32,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Bounds {
-    pub min_x: f32, 
+    pub min_x: f32,
     pub max_x: f32,
     pub min_y: f32,
-    pub max_y: f32
+    pub max_y: f32,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -20,5 +19,5 @@ pub struct Summary {
     pub title: String,
     pub description: String,
     pub bounds: Bounds,
-    pub center: Center
+    pub center: Center,
 }
